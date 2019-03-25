5e6e1125a3a3164b7031242a1dabed6d6ffe38ae
William <william@pixney.com>
2019-03-25 19:17:19 +0100

    Vertical tab

---
+++ resources/config/sections.php
@@ -0,0 +1,19 @@
+
+<?php
+
+return [
+    'block' => [
+        'fields' => []
+    ],
+    'options' => [
+        'stacked' => true,
+        'tabs'    => [
+            'offices'     => [
+                'title'  => 'Offices',
+                'fields' => [
+                    'entry_offices',
+                ],
+            ]
+        ],
+    ]
+];
